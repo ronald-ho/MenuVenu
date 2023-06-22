9de12daaee23210b87dc6eb54276b8159a9353bf
z5313344 <z5313344@gitlab.cse.unsw.edu.au>
2023-06-23 00:26:13 +1000

    added instructions on how to start project

--- project/backend/app/__init__.py
+++ project/backend/app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail, Message
-import os
+from flask_mail import Mail
 
 app = Flask(__name__)
-db_path = os.path.join(os.path.dirname(__file__), 'mydatabase.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
 
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://MenuVenu:MenuVenu@db:5432/MenuVenu'
+app.config['SQLALCHEMY_POOL_SIZE'] = 20
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 5
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+
+# Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
@@ -15,7 +20,3 @@ app.config['MAIL_PASSWORD'] = "" # TODO: Configure our email password
 
 db = SQLAlchemy(app)
 mail = Mail(app)
-
-if not os.path.exists(db_path):
-    with app.app_context():
-        db.create_all()
