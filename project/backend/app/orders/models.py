a3240d4f272dae29361c2b78580fce97d2c34443
Daniel Ouyang <danielouyang337@gmail.com>
2023-06-23 19:11:33 +1000

    MOG-56 finished data model for OrderedItems

--- project/backend/app/orders/models.py
+++ project/backend/app/orders/models.py
@@ -1,6 +1,8 @@
 from .. import db
 from dataclasses import dataclass
 from sqlalchemy.sql import func
+from app.authentication.models import Customers
+from app.menu.models import MenuItems
 
 @dataclass
 class DiningTables(db.model):
@@ -20,5 +22,6 @@ class Orders(db.model):
 class OrderedItems(db):
     order_item_id = db.Column(db.Integer, primary_key = True)
     order_id = db.Column(db.Integer, db.ForeignKey(Orders.order_id), primary_key = True)
-    #customer_id = db.Column(db.Integer, db.ForeignKey(Customers.customer_id), primary_key = True)
-    #item_id = db.Column(db.Integer, db.ForeignKey(MenuItems.item_id), primary_key = True)
+    customer_id = db.Column(db.Integer, db.ForeignKey(Customers.customer_id), primary_key = True)
+    item_id = db.Column(db.Integer, db.ForeignKey(MenuItems.item_id), primary_key = True)
+    
