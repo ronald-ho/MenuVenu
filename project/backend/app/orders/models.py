c5f42a3501cbb0317418ce706705dbe9df322d42
Daniel Ouyang <danielouyang337@gmail.com>
2023-06-23 18:52:44 +1000

    MOG-56 added data models for DiningTables, Orders, OrderedItems

---
+++ project/backend/app/orders/models.py
@@ -0,0 +1,24 @@
+from .. import db
+from dataclasses import dataclass
+from sqlalchemy.sql import func
+
+@dataclass
+class DiningTables(db.model):
+    table_id =          db.Column(db.Integer, primary_key = True)
+    table_number =      db.Column(db.Integer, unique = True, nullable = False)
+    is_ocuppied =       db.Column(db.Boolean, default = False)
+    needs_assistance =  db.Column(db.Boolean, default = False)
+
+@dataclass
+class Orders(db.model):
+    order_id = db.Column(db.Integer, primary_key = True)
+    table_id = db.Column(db.Integer, db.ForeignKey(DiningTables.table_id), primary_key = True)
+    order_date = db.Column(db.DateTime(timezone = True), server_default = func.now(), nullable = False)
+    total_amount = db.Column(db.Float, nullable = False)
+
+@dataclass
+class OrderedItems(db):
+    order_item_id = db.Column(db.Integer, primary_key = True)
+    order_id = db.Column(db.Integer, db.ForeignKey(Oders.order_id), primary_key = True)
+    #customer_id = db.Column(db.Integer, db.ForeignKey(Customers.customer_id), primary_key = True)
+    #item_id = db.Column(db.Integer, db.ForeignKey(MenuItems.item_id), primary_key = True)
