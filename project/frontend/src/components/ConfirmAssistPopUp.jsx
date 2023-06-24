820842655f16d25b3a412ef7f7071e80a7df8c5e
emilytangg <emilytang3012@gmail.com>
2023-06-24 19:12:06 +1000

    MOG-55 Implemented polling for tables requiring assistance

--- project/frontend/src/components/ConfirmAssistPopUp.jsx
+++ project/frontend/src/components/ConfirmAssistPopUp.jsx
@@ -1,15 +1,17 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import React from "react";
+import { TablesReqAssistPolling } from "../pages/WaitStaff";
+import { apiCall } from "../helpers/helpers";
 
 function ConfirmAssistPopUp ({ open, setOpen, table}) {
     async function handleConfirm() {
         const data = await apiCall("orders/finish_assist", "POST", {table_number: table});
         if (data.message === "Assistance completed") {
-            // remove table from column or
-            // likely reload a list of all tables needing assist
+            // Reload a list of all tables needing assist
+            TablesReqAssistPolling();
         } 
         else {
-            // setShowAlert(data.message);
+            console.log("failed to fetch tables");
         }
     }
 
--- project/frontend/src/pages/WaitStaff.jsx
+++ project/frontend/src/pages/WaitStaff.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import { Box, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 import AssistReqTableButton from "../components/AssistReqTableButton";
 import ConfirmAssistPopUp from "../components/ConfirmAssistPopUp";
+import { apiCall } from "../helpers/helpers";
 
 function WaitStaff () {
-    const [showConfirmAssist, setShowConfirmAssist] = React.useState(false);
-
     // ORDER ITEM READY 
     // 1. Poll for new items to be served and show in table
     // 2. If waitstaff clicks table in Table no. column, pop up appears confirming whether food served or not
     // 3. If no, do nothing. If yes, send API call to show item was served. Remove item from list
 
     // ASSISTANCE REQUIRED
-    // 1. Poll for tables needing assistance -> in db will have "assistance" field etc.
-    // 2. If needs assistance, show in the column
-    // 3. If waitstaff clicks table in column, pop up appears confirming whether assisted or not
-    // 4. If no, do nothing. If yes, send API call to show table has been assisted. Remove table from the column
-
-    // loop through tables req assistance and display buttons in column
-    // poll to reload and reflect changes
+    // 1. Poll for tables needing assistance and show in the column
+    // 2. If waitstaff clicks table in column, pop up appears confirming whether assisted or not
+    // 3. If no, do nothing. If yes, send API call to show table has been assisted. Remove table from the column
+       
+    TablesReqAssistPolling();
 
     return (
         <>
@@ -47,21 +44,69 @@ function WaitStaff () {
                     height: '85vh',
                 }}
             >
-                <Typography sx={{
-                    fontWeight: 'bold',
-                    padding: '15px 10px',
-                }}>ASSISTANCE REQUIRED AT</Typography>
-                <Box sx={{ 
-                    height: '74vh',
-                    overflow: 'auto' 
-                }}>
-                    <AssistReqTableButton handleClick={() => setShowConfirmAssist(true)} table={1}></AssistReqTableButton>
-                    <AssistReqTableButton handleClick={() => setShowConfirmAssist(true)} table={2}></AssistReqTableButton>
+                <Typography sx={{ fontWeight: 'bold', padding: '15px 10px' }}>
+                    ASSISTANCE REQUIRED AT
+                </Typography>
+            </Box>
+        </>
+    );
+}
+
+export const TablesReqAssistPolling = () => {
+    const [tableData, setTableData] = React.useState([]);
+    const [showConfirmAssist, setShowConfirmAssist] = React.useState(false);
+
+    React.useEffect(() => {
+        const fetchTableData = async () => {
+            // Make API call to fetch tables requiring assistance
+            const data = await apiCall("orders/get_assist", "GET", {});
+            if (data.table_number_list) {
+                setTableData(data.table_number_list);
+            }
+            else {
+                console.log("Failed to fetch tables req assistance");
+            }
+        };
+
+        // Set up polling with a 5-second interval
+        const pollingInterval = setInterval(fetchTableData, 5000);
+
+        // Clean up the interval when the component unmounts
+        return () => clearInterval(pollingInterval);
+    }, []);
+
+    return (
+        <>
+            <Box
+                sx={{
+                    border: 1,
+                    borderTop: 0,
+                    fontSize: '15px',
+                    position: 'absolute',
+                    textAlign: 'center',
+                    top: '81.6px',
+                    right: 0,
+                    width: '140px',
+                    height: '85vh',
+                }}
+            >
+                <Typography sx={{ fontWeight: 'bold', padding: '15px 10px' }}>
+                    ASSISTANCE REQUIRED AT
+                </Typography>
+                <Box sx={{ height: '74vh', overflow: 'auto' }}>
+                    {tableData.map((table) => (
+                        <AssistReqTableButton 
+                            key={table.table_id} 
+                            handleClick={() => setShowConfirmAssist(true)} 
+                            table={table}
+                        >
+                        </AssistReqTableButton>
+                    ))}
                 </Box>
             </Box>
             {showConfirmAssist && <ConfirmAssistPopUp open={showConfirmAssist} setOpen={setShowConfirmAssist} table={1}/>}
         </>
     );
-}
+};
 
 export default WaitStaff;
