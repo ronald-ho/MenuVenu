892bdfbc2473625ce82f3149a78e1b92648dec87
ronald-ho <ronaldho1202@gmail.com>
2023-06-25 13:02:55 +1000

    changed the customer model

--- project/backend/app/authentication/models.py
+++ project/backend/app/authentication/models.py
@@ -4,16 +4,15 @@ from .. import db
 class Customers(db.Model):
     customer_id     = db.Column(db.Integer, primary_key = True)
     email           = db.Column(db.String(120), unique = True, nullable = False)
-    full_name        = db.Column(db.String(120), nullable = False)
+    full_name       = db.Column(db.String(120), nullable = False)
     password        = db.Column(db.String(255), nullable = False)
     points          = db.Column(db.Integer, nullable = False)
     calories_burnt  = db.Column(db.Integer, nullable = False)
     calories_gained = db.Column(db.Integer, nullable = False)
-    reset_code      = db.Column(db.String(6), nullable = True)
+    reset_code      = None
 
     def generate_reset_code(self):
         self.reset_code = random.randint(100000, 999999)
-        db.session.commit()
         return self.reset_code
 
 if __name__ == '__main__':
