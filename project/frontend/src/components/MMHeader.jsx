74943e9e8f93b463cf485a52f7ad167c1fd6e915
unknown <anthony.lin4321@gmail.com>
2023-06-27 20:33:34 +1000

    log out

--- project/frontend/src/components/MMHeader.jsx
+++ project/frontend/src/components/MMHeader.jsx
@@ -9,12 +9,12 @@ const StyledHeader = styled.header({
   border: '1px black solid'
 });
 
-function MMHeader () {
+function MMHeader ({ mode, setmode }) {
   return (
     <StyledHeader>
       <Typography sx={{ fontSize: '40px', padding: '10px 20px' }}>MOGGER MEALS</Typography>
       <div>
-        {localStorage.getItem("mvuser") && <LogOutButton />}
+        {mode === 'customer' && <LogOutButton setmode={setmode}/>}
       </div>
     </StyledHeader>
   )
