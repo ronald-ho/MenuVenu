6d19888528e474c31c118a0080610ba97fd00731
ronald-ho <ronaldho1202@gmail.com>
2023-07-04 00:39:13 +1000

    MOG-78 finished get item, categories, update position, and get item details, updated menu model

--- project/backend/app/menu/routes.py
+++ project/backend/app/menu/routes.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 
 from app import app, db
+from .models import Items, Categories, Ingredients, Contains
 
 @app.route('/menu/add/categories', methods=['POST'])
 def add_categories():
@@ -30,40 +31,80 @@ def delete_categories():
     """
     pass
 
-@app.route('/menu/items', methods=['GET'])
-def get_items():
-    """
-    Get items from menu
-    """
-    pass
+@app.route('/menu/items/<category_id>', methods=['GET'])
+def get_items(category_id):
+
+    app.logger.info(f"category_id: {category_id}")
+
+    category = Categories.query.get(category_id)
+
+    if not category:
+        return jsonify({'status': 404, 'message': 'Category not found'})
+    
+    item_list = []
+
+    for item in category.items:
+        item_list.append(item.to_dict())
+
+    return jsonify({'status': 200, 'message': 'Items found', 'items': item_list})
+    
 
 @app.route('/menu/categories', methods=['GET'])
 def get_categories():
-    """
-    Get categories from menu
-    """
-    pass
+    
+    app.logger.info("Getting categories from database")
+
+    categories = Categories.query.all()
+
+    category_list = [categories.to_dict() for categories in categories]
+
+    return jsonify({'status': 200, 'message': 'Categories found', 'categories': category_list})
+
 
 @app.route('/menu/item/position', methods=['PUT'])
 def update_item_position():
-    """
-    Update item position in menu
-    """
-    pass
+    
+    data = request.get_json()
+    app.logger.info(f"data: {data}")
+
+    item = Items.query.get(data['item_id'])
+
+    if not item:
+        return jsonify({'status': 404, 'message': 'Item not found'})
+    
+    item.position = data['position']
+    db.session.commit()
+
+    return jsonify({'status': 200, 'message': 'Item position updated'})
+
 
 @app.route('/menu/category/position', methods=['PUT'])
 def update_category_position():
-    """
-    Update category position in menu
-    """
-    pass
 
-@app.route('/menu/item/details', methods=['GET'])
-def get_item_details():
-    """
-    Get item details
-    """
-    pass
+    data = request.get_json()
+    app.logger.info(f"data: {data}")
+
+    category = Categories.query.get(data['category_id'])
+
+    if not category:
+        return jsonify({'status': 404, 'message': 'Category not found'})
+    
+    category.position = data['position']
+    db.session.commit()
+
+    return jsonify({'status': 200, 'message': 'Category position updated'})
+
+@app.route('/menu/item/details/<item_id>', methods=['GET'])
+def get_item_details(item_id):
+    
+    app.logger.info("Getting item details from database")
+
+    item = Items.query.get(item_id)
+
+    if not item:
+        return jsonify({'status': 404, 'message': 'Item not found'})
+    
+    return jsonify({'status': 200, 'message': 'Item found', 'item': item.to_dict()})
 
 @app.route('/menu/items', methods=['PUT'])
 def update_items():
