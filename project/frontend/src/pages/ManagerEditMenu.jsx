e407c7152773b2983d75f245f0d39496d7c74213
emilytangg <emilytang3012@gmail.com>
2023-07-06 18:27:49 +1000

    MOG-81 Added add category pop up

--- project/frontend/src/pages/ManagerEditMenu.jsx
+++ project/frontend/src/pages/ManagerEditMenu.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Box, Button } from "@mui/material";
+import AddCategoryPopUp from "../components/AddCategoryPopUp";
 
 function ManagerEditMenu () {
+    // on first load, get all categories and render 
+    const [openAddCategory, setOpenAddCategory] = React.useState(false);
+
     return (
         <Box sx={{
             border: 1,
@@ -16,13 +20,14 @@ function ManagerEditMenu () {
             <Box sx={{ borderRight: 1, width: "20vw" }}>
                 <Box sx={{ borderBottom: 1, padding: "15px 0"}}>CATEGORIES</Box>
                 <Box>
-                    Categories will be inserted here
+                    {/* After each add, deletion, loop through and rerender list of categories? */}
                 </Box>
-                <Button color="success" variant="outlined">+ New Category</Button>
+                <Button onClick={() => setOpenAddCategory(true)} sx={{ margin: "10px" }} color="success" variant="outlined">+ New Category</Button>
             </Box>
             <Box sx={{ width: "50vw" }}>
                 <Box sx={{ borderBottom: 1, padding: "15px 0" }}>MENU ITEMS</Box>
             </Box>
+            {openAddCategory && <AddCategoryPopUp open={openAddCategory} setOpen={setOpenAddCategory}/>}
         </Box>
     )
 }
