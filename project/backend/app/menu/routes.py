1442ab38ca96148918df745de48fc1801083a1a7
ronald-ho <ronaldho1202@gmail.com>
2023-07-10 22:25:12 +1000

    removed import

--- project/backend/app/menu/routes.py
+++ project/backend/app/menu/routes.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from flask import request, jsonify, Blueprint
 
 from .models import Items, Categories
-from .services import ItemService, CategoryService, MenuService
+from .services import ItemService, CategoryService
 from .. import app, db
 
 menu = Blueprint('menu', __name__)
