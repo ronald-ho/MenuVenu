10ec91e10054181ba4a8a2f08f69b8db8f44d450
unknown <anthony.lin4321@gmail.com>
2023-07-14 21:50:45 +1000

    fixed problem with route

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -104,7 +104,7 @@ function App() {
         path: "/kitchen",
         element: <KitchenOrders />
       }, {
-        path: "/kitchenlogin",
+        path: "/kitchenstafflogin",
         element: <KitchenStaffLogin />
       }
     ]
