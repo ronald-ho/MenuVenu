4bf8af061036f390b637f2fb77c6ec46a5e4d816
ronald-ho <ronaldho1202@gmail.com>
2023-07-15 16:06:57 +1000

    removed creating instances of classes

--- project/backend/app/orders/routes.py
+++ project/backend/app/orders/routes.py
@@ -3,95 +3,90 @@ from flask import request
 from .services import AssistService, TableService, OrderService
 from .. import app
 
-assist_service = AssistService()
-table_service = TableService()
-order_service = OrderService()
-
-
 @app.route('/orders/req_assist', methods=['POST'])
 def req_assist():
     data = data_logger(request)
 
-    return assist_service.request_assist(data)
+    return AssistService.request_assist(data)
 
 
 @app.route('/orders/finish_assist', methods=['POST'])
 def finish_assist():
     data = data_logger(request)
 
-    return assist_service.finish_assist(data)
+    return AssistService.finish_assist(data)
 
 
 @app.route('/orders/get_assist', methods=['GET'])
 def get_assist():
-    return assist_service.get_assist()
+    return AssistService.get_assist()
 
 
 @app.route('/orders/pay_bill', methods=['POST'])
 def pay_bill():
     data = data_logger(request)
 
-    return table_service.pay_bill(data)
+    return TableService.pay_bill(data)
 
 
 @app.route('/orders/get_bill', methods=['POST'])
 def get_bill():
     data = data_logger(request)
 
-    return table_service.get_bill(data)
+    return TableService.get_bill(data)
 
 
 @app.route('/orders/select_table', methods=['POST'])
 def select_table():
     data = data_logger(request)
 
-    return table_service.select_table(data)
+    return TableService.select_table(data)
 
 
 @app.route('/orders/get_tables', methods=['GET'])
 def get_tables():
 
-    return table_service.get_tables()
+    return TableService.get_tables()
 
 
 @app.route('/orders/order_item', methods=['POST'])
 def order_item():
     data = data_logger(request)
 
-    return order_service.order_item(data)
+    return OrderService.order_item(data)
 
 
 @app.route('/orders/kitchen/prepared', methods=['POST'])
 def kitchen_prepared():
     data = data_logger(request)
 
-    return order_service.kitchen_prepared(data)
+    return OrderService.kitchen_prepared(data)
 
 
 @app.route('/orders/get_order_list', methods=['GET'])
 def get_order_list():
 
-    return order_service.get_order_list()
+    return OrderService.get_order_list()
 
 
 @app.route('/orders/get_ordered_items', methods=['POST'])
 def get_ordered_items():
     data = data_logger(request)
 
-    return order_service.get_ordered_items(data)
+    return OrderService.get_ordered_items(data)
 
 
 @app.route('/orders/waitstaff/served', methods=['POST'])
 def waitstaff_served():
     data = data_logger(request)
 
-    return order_service.waitstaff_served(data)
+    return OrderService.waitstaff_served(data)
 
 
 @app.route('/orders/get_serve_list', methods=['GET'])
 def get_serve_list():
 
-    return order_service.get_serve_list()
+    return OrderService.get_serve_list()
 
 
 def data_logger(request):
