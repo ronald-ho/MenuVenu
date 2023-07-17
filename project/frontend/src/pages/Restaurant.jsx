602b93a3411b3b737450122ce44babe2b4745d68
ronald-ho <ronaldho1202@gmail.com>
2023-07-18 00:04:16 +1000

    added icons to main page

--- project/frontend/src/pages/Restaurant.jsx
+++ project/frontend/src/pages/Restaurant.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";  
-import { Typography, Box } from "@mui/material";
+import {Link} from "react-router-dom";
+import {Box, Button, Typography} from "@mui/material";
+import SoupKitchenIcon from '@mui/icons-material/SoupKitchen';
+import FaceIcon from '@mui/icons-material/Face';
 
 function Restaurant () {
     return (
         <>
         <div style={{textAlign: 'center'}}>
             <Typography>I am a...</Typography>
-            <Box component={Link} to={'/customerselect'} sx={{ display: 'inline-block', width: '20%', padding: '10px', border: '1px black solid', margin: '10px' }}>
-                <Typography>Customer</Typography>
-            </Box>
-            <Box component={Link} to={'/staffselect'} sx={{ display: 'inline-block', width: '20%', padding: '10px', border: '1px black solid', margin: '10px' }}>
-                <Typography>Staff Member</Typography>    
+            <Box sx={{ display: 'inline-flex', justifyContent: 'center', gap: '30px' }}>
+                <Button component={Link} to="/customerselect" variant="contained" size="large" sx={{width: "200px", color: "white"}}>
+                    <FaceIcon sx={{mr: 2, fontSize: 30}} />
+                    <Typography>Customer</Typography>
+                </Button>
+                <Button component={Link} to="/staffselect" variant="contained" size="large" sx={{width: "200px", color: "white"}}>
+                    <SoupKitchenIcon sx={{mr: 1, fontSize: 30}} />
+                    <Typography>Staff Member</Typography>
+                </Button>
             </Box>
         </div>
         </>
