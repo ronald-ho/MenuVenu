07d399c6c9e10f93acca8aba5d1d868e07845fde
Jim Huang <z5310201@ad.unsw.edu.au>
2023-07-25 22:32:54 +1000

    MOG-103 accounted for filter and category id

--- project/backend/app/restaurant/routes.py
+++ project/backend/app/restaurant/routes.py
@@ -151,3 +151,105 @@ def get_orderlog():
         return jsonify({'error': str(e)}), 500
 
 
+@app.route('/manager/profit_tracker', methods=['GET'])
+def get_profit():
+    timespan = request.args.get('time')
+    fil = request.args.get('filter')
+    category_id = request.args.get('category_id')
+
+    try:
+        
+
+        
+        if timespan == 'week':
+            start_time = end_time - timedelta(days=7)
+        elif timespan == 'month':
+            start_time = end_time - timedelta(days=30)
+        elif timespan == 'year':
+            start_time = end_time - timedelta(days=365)
+        elif timespan == 'all':
+            start_time = datetime.min
+        
+        else:
+            return jsonify({'status': HTTPStatus.BAD_REQUEST, 'message': 'invalid range'})
+            
+        order_log = db.session.query(
+            Orders.id.label('order_id'),
+            OrderedItems.id.label('ordered_item_id'),
+            Items.id.label('item_id'),
+            Items.name.label('item_name'),
+            func.count(OrderedItems.item).label('item_popularity'),
+            case((OrderedItems.redeemed == True, 0),\
+            else_=Items.price).label('item_price'),
+            case([(OrderedItems.redeemed == True, 0 - Items.price)],\
+            else_=Items.net).label('item_net')             
+            OrderedItems.redeemed.label('item_redeemed')
+            Items.category.label('category_id')
+        )\
+        .join(OrderedItems, Orders.id == OrderedItems.order)\
+        .join(Items, OrderedItems.item == Items.id)\
+        .filter(
+            Orders.order_date >= start_time,
+            Orders.order_date <= end_time,
+            Orders.paid == True,
+        )\
+        .group_by(Orders.id,
+            OrderedItems.id,
+            Items.id,
+            Items.name,
+            OrderedItems.redeemed,
+            Items.category)\
+        .all()
+
+        order_log_list = [
+                {
+                    'order_id': row.order_id,
+                    'ordered_item_id': row.ordered_item_id,
+                    'item_id': row.item_id,
+                    'item_name': row.item_name,
+                    'item_popularity': row.item_popularity,
+                    'item_price': row.item_price,
+                    'item_net': row.item_net,
+                    'item_redeemed': row.item_redeemed,
+                    'item_category': row.category_id
+                }
+                for row in order_log
+            ]
+
+        order_log_list = sorted(order_log_list, key = lambda x: x['order_id'])
+
+        if category_id:
+            order_log_list = list(filter(lambda item: item.get('item_category') == category_id, order_log_list))
+
+        
+        
+        if fil == 'popularity':
+            total_pop = sum(row['item_popularity'] for row in order_log_list)
+            pop = {timespan: total_pop}
+
+            return jsonify({'status': HTTPStatus.OK, 'popularity': pop})
+
+        elif fil == 'gross': 
+            total_income = sum(row['item_price'] for row in order_log_list)
+            total = {timespan: total_income}  
+            
+            return jsonify({'status': HTTPStatus.OK, 'gross_income': total})
+
+        elif fil == 'net':
+            net_income = sum(row['item_net'] for row in order_log_list)
+            net = {timespan: net_income}
+
+            return jsonify({'status': HTTPStatus.OK, 'net_income': net_income})
+        
+        else:
+            return jsonify({'status': HTTPStatus.NOT_FOUND, 'message': 'Filter not found'})
+
+'''
+        order_log_list = sorted(order_log_list, key = lambda x: x['order_id'])
+
+        return jsonify({'status': HTTPStatus.OK, 'orderlog': order_log_list, 'gross_income': total})
+'''
+
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
