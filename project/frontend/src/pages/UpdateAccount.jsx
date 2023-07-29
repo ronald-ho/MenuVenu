34684bbd2972fe92ebb9047a7e6e4c27aa4032cd
emilytangg <emilytang3012@gmail.com>
2023-07-29 17:30:14 +1000

    Update account page rerenders accordingly once connect to Google Fit

--- project/frontend/src/pages/UpdateAccount.jsx
+++ project/frontend/src/pages/UpdateAccount.jsx
@@ -1,53 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { Link, useLoaderData } from "react-router-dom";
-import UpdateIcon from '@mui/icons-material/Update';
-import DeleteIcon from '@mui/icons-material/Delete';
-import GoogleIcon from '@mui/icons-material/Google';
+import { Delete, Google, Update } from '@mui/icons-material';
+import { apiCall } from "../helpers/helpers";
+import { get_profile } from "../helpers/loaderfunctions";
 
 function UpdateAccount() {
-    const details = useLoaderData();
-    const [isSignedIn, setIsSignedIn] = useState(false); // Add this state
+    const [details, setDetails] = React.useState(useLoaderData());
+    const [isSignedIn, setIsSignedIn] = React.useState(details.google_connected);
 
-    function handleCredentialResponse(response) {
+    const client = google.accounts.oauth2.initTokenClient({
+        client_id: "155679089529-vgjnspusl7a28vt5m4r0jsu2o31rvdhq.apps.googleusercontent.com",
+        scope: "https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.activity.write https://www.googleapis.com/auth/fitness.nutrition.write",
+        callback: handleCredentialResponse,
+    });
+
+    React.useEffect(() => {
+        async function getCustomerDetails () {
+            const data = await get_profile();
+            setDetails(data);
+        }
+
+        getCustomerDetails();
+        
+    }, [isSignedIn]);
+
+    async function handleCredentialResponse(response) {
         console.log(response);
         if (response.error) {
             setIsSignedIn(false);
-        } else {
-            setIsSignedIn(true);
-
+        } 
+        else {
             const mvuser = localStorage.getItem("mvuser")
 
-            const request = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    token: response.access_token,
-                    expires_in: response.expires_in,
-                    id: mvuser,
-                })
-            };
+            const body = {
+                token: response.access_token,
+                expires_in: response.expires_in,
+                id: mvuser,
+            }
 
-            fetch("http://127.0.0.1:5000/fitness/token/store", request)
-                .then((res) => res.json())
-                .then((data) => console.log(data));
+            const data = await apiCall('/fitness/token/store', 'POST', body)
+            console.log(data);
+            setIsSignedIn(true);
         }
-
-
     }
-
-    useEffect(() => {
-        /* global google */
-        const client = google.accounts.oauth2.initTokenClient({
-            client_id: "155679089529-vgjnspusl7a28vt5m4r0jsu2o31rvdhq.apps.googleusercontent.com",
-            scope: "https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.activity.write https://www.googleapis.com/auth/fitness.nutrition.write",
-            callback: handleCredentialResponse,
-        });
-
-        document.getElementById("authorizeButton").addEventListener("click", () => {
-            client.requestAccessToken();
-        });
-    }, []);
 
     return (
         <>
@@ -56,23 +52,25 @@ function UpdateAccount() {
                 <Typography variant="h2">{details.full_name}</Typography>
                 <Typography>Email address: {details.email}</Typography>
                 <Typography>MV Points: {details.points}</Typography>
+                {isSignedIn && <Typography>Calories burned: {details.calories_burnt}</Typography>}
                 <Typography sx={{ marginBottom: '20px' }}>Google Fit: {isSignedIn ? 'Connected' : 'Unconnected'}</Typography>
                 <Box sx={{ display: 'inline-flex', justifyContent: 'center', gap: '30px' }}>
                     <Button component={Link} to={"/changedetails"} variant="contained" size="large" sx={{ width: '300px' }}>
-                        <UpdateIcon sx={{ mr: 1, fontSize: 30 }} />
+                        <Update sx={{ mr: 1, fontSize: 30 }} />
                         <Typography>Update Account</Typography>
                     </Button>
-                    <Button id="authorizeButton" component={Link} variant="contained" size="large" sx={{ width: '300px' }}>
-                        {isSignedIn ?
-                        <Typography>Google Fit Connected</Typography> :
-                        <>
-                        <GoogleIcon sx={{mr: 1, fontSize: 30}} />
-                        <Typography>Connect Google Fit</Typography>
-                        </>
-                        }
+                    <Button onClick={() => client.requestAccessToken()} component={Link} variant="contained" size="large" sx={{ width: '300px' }}>
+                        {isSignedIn ? (
+                            <Typography>Google Fit Connected</Typography>
+                        ) : (
+                            <>
+                                <Google sx={{mr: 1, fontSize: 30}} />
+                                <Typography>Connect Google Fit</Typography>
+                            </>
+                        )}
                     </Button>
                     <Button component={Link} to={"/deleteaccount"} variant="contained" size="large" sx={{ width: '300px' }}>
-                        <DeleteIcon sx={{ mr: 1, fontSize: 30 }} />
+                        <Delete sx={{ mr: 1, fontSize: 30 }} />
                         <Typography>Delete Account</Typography>
                     </Button>
                 </Box>
