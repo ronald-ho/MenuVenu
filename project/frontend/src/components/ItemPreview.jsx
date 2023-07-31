0ed46f080a8616a43bb760cca23993644443cb23
emilytangg <emilytang3012@gmail.com>
2023-08-01 02:03:57 +1000

    Styling changes to menu item preview

--- project/frontend/src/components/ItemPreview.jsx
+++ project/frontend/src/components/ItemPreview.jsx
@@ -8,18 +8,44 @@ function ItemPreview({ item }) {
             component={Link} to={"../order/" + item.id} 
             sx={{ 
                 border: "1px solid #caccce",
-                height: "200px",
+                display: 'flex',
+                flexDirection: 'column',
+                height: "270px",
                 margin: "5px", 
                 padding: "5px",
-                width: "200px"
+                textDecoration: 'none',
+                width: "270px"
             }}
         >
             <Typography>{item.name}</Typography>
-            {item.image && <img src={item.image} alt={item.name} style={{maxWidth: "100px", maxHeight: "100px"}}/>}
-            <Typography>${item.price}</Typography>
-            {item.points_to_redeem && <Typography>{item.points_to_redeem} MV Points</Typography>}
-            {item.calories && <Typography>{item.calories} Cal</Typography>}
-            {item.ingredients.map((i) => ( <Chip label={i} /> ))}
+            <Box sx={{ border: "1px solid #caccce", display: "flex", height: "150px", margin: 'auto', width: "150px" }}>
+                {item.image ? (
+                    <img src={item.image} alt={item.name} style={{maxWidth: "150px", maxHeight: "150px"}}/>
+                ) : (
+                    <Typography 
+                        sx={{ 
+                            fontSize: '100px', 
+                            fontWeight: 700, 
+                            opacity: '0.1' 
+                        }}
+                    >
+                        MV
+                    </Typography>
+                )
+                }
+            </Box>
+            <Typography>
+                ${item.price.toFixed(2)} 
+                {item.points_to_redeem && <> | + {item.points_to_redeem} MVP</>}
+                {item.points_to_redeem && item.points_earned && <> | </>}
+                {item.points_earned && <>- {item.points_earned} MVP</>}
+            </Typography>
+            <Box>
+                {item.calories && <Typography>{item.calories} Cal</Typography>}
+                {item.ingredients.map((i) => ( <Chip label={i} /> ))}
+            </Box>
+            
+         
         </Card>
     )
 }
